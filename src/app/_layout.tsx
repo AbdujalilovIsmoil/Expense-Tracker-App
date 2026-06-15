c0d1a17e2a6f79eca0e4f8bc23734ca8ab6aa846
Ismoil Abdujalilov <ismoilbek20050421@gmail.com>
2026-06-15 11:22:01 -0700

    feat: Added Styling The Expense Management Screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,23 @@
+import { GlobalStyles } from "@/constants/styles";
 import { Stack } from "expo-router";
 
 const Layout = () => {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        headerTintColor: "white",
+        headerStyle: {
+          backgroundColor: GlobalStyles.colors.primary500,
+        },
+      }}
+    >
       <Stack.Screen
         name="manage-expense"
         options={{
           headerShown: true,
+          presentation: "modal",
+          title: "Manage Expense",
           headerBackTitle: "Back",
         }}
       />
