6f41a7d206bb7525c8fd0db5cde649c3a3ad4686
Ismoil Abdujalilov <ismoilbek20050421@gmail.com>
2026-06-15 12:01:49 -0700

    feat: Adding a 'Delete' Button

--- src/app/manage-expense.tsx
+++ src/app/manage-expense.tsx
@@ -1,7 +1,8 @@
+import IconButton from "@/components/UI/IconButton";
+import { GlobalStyles } from "@/constants/styles";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, View } from "react-native";
 
 const ManageExpense = () => {
   const navigation = useNavigation();
@@ -17,11 +18,37 @@ const ManageExpense = () => {
     });
   }, [navigation, isEditing]);
 
+  const deleteExpenseHandler = () => {};
+
   return (
-    <SafeAreaView>
-      <Text>Manage Expense</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            size={36}
+            icon="trash"
+            onPress={deleteExpenseHandler}
+            color={GlobalStyles.colors.error500}
+          />
+        </View>
+      )}
+    </View>
   );
 };
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    alignItems: "center",
+    borderTopColor: GlobalStyles.colors.primary200,
+  },
+});
